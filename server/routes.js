a26befb66a979877c9a411690d4a4a59ce600712
Emmie Salama <emmie.salama@gmail.com>
2015-01-13 10:32:12 -0500

    add cart model

--- server/routes.js
+++ server/routes.js
@@ -9,6 +9,7 @@ var errors = require('./components/errors');
 module.exports = function(app) {
 
   // Insert routes below
+  app.use('/api/cart', require('./api/cart'));
 
   app.use('/api/categorys', require('./api/category'));
   app.use('/api/products', require('./api/product'));
