ff0aaf628998d66c6c8675107305f48f9f9dab3b
Michael J. Abraham <mjabraham@gmail.com>
2015-01-15 15:39:19 -0500

    search bar works

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,14 +3,22 @@
 angular.module('potterBarnApp')
   .controller('MainCtrl', function ($scope, $http, socket, $state, $stateParams) {
     $scope.categories = [];
+    $scope.products = [];
 
     //List of categories on main page
     $http.get('/api/categorys').success(function(categories) {
       $scope.categories = categories;
     });
+
+    $http.get('/api/products/').success(function(product) {
+      $scope.products = product;
+    });
     
     //Keeps category that is pressed
     $scope.stateChange = function(category) {
     	$state.go("category", {'category': category})
+    };
+    $scope.getProduct = function(product) {
+      $state.go("product", {'product': product})
     };
   });
