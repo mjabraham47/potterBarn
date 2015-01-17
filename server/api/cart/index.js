cb30d275efd785b57c7559cf275d5e8ac5ecbee9
Michael J. Abraham <mjabraham@gmail.com>
2015-01-17 13:05:33 -0500

    quantity changes well

--- server/api/cart/index.js
+++ server/api/cart/index.js
@@ -11,7 +11,9 @@ router.post('/', controller.create);
 router.put('/:id', controller.update);
 router.patch('/:id', controller.update);
 router.delete('/:id', controller.destroy);
-router.delete('/:id/:item', controller.destroyProduct)
+router.get('/:id/:item/:quantity', controller.getQuantity);
+router.post('/:id/:item/:quantity', controller.updateQuantity);
+router.delete('/:id/:item', controller.destroyProduct);
 router.get('/newcart/:id', controller.create_new_user_cart);
 router.get('/add/:id/:product/:quantity', controller.add_product);
 
