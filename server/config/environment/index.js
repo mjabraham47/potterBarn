17e2abc31752423c6ae0e2925c7eb6f9c2904a2f
Emmie Salama <emmie.salama@gmail.com>
2015-01-20 17:24:40 -0500

    google integration works

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -44,7 +44,7 @@ var all = {
   google: {
     clientID:     process.env.GOOGLE_ID || 'id',
     clientSecret: process.env.GOOGLE_SECRET || 'secret',
-    callbackURL:  (process.env.DOMAIN || '') + '/auth/google/callback'
+    callbackURL:  '/auth/google/callback'
   }
 };
 
